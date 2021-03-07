c6f69f90ce14b56b9b172b29c5cda5775fecf3b0
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-07 19:46:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,15 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-min_value = nil
+value = nil
+min_value = 10000
 min_value_name = nil
 
 name_hash.each do |key, value|
-    if name_hash[key][value] = min_value
-      or name_hash[key][value] <=
+    if name_hash[key][value] = value
+      or name_hash[key][value] <= min_value
       min_value = name_hash[key][value]
     end
+    min_value[key]
   end
 end
