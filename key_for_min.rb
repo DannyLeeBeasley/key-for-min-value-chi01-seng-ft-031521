ef14c589617894a4e7def0357c7124308f033d7e
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-07 20:30:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,7 @@ min_value_name = nil
 
 name_hash.each do |key, value|
   binding.pry
-#     if name_hash[key][value] = nil
-#       #or name_hash[key][value] <= min_value
+#     if name_hash[key][value] <= min_value
 #       min_value = name_hash[key][value]
 #     end
 #     min_value_name = min_value[key]
