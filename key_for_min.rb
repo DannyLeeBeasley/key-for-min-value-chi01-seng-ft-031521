b4585a1e722a7a2c4121cdb2d013aa3972987415
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-07 19:34:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
+min_value = 100 
 
+name_hash.each do |key, value|
+  name_hash[key][value]
+    if name_hash[key][value] <= min_value 
+      min_value = name_hash[key][value]
+    end
+  end
 end
